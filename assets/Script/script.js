3d77709cdaa6b252c26c5bb15aafa49bccab68ee
MALWICK <batahumphrey66@gamil.com>
2023-08-11 11:29:27 +0100

    created new card

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,9 +1,11 @@
 const app = Vue.createApp({
-    template: `<h1>Hello {{firstname}} {{lastName}}</h1>`,
   data() {
     return {
       firstname: "John",
       lastName: "Smith",
+      email: "JohnSmith@gmail.com",
+      gender:'male',
+      picture: "https://randomuser.me/api/portraits/men/10.jpg",
     };
   },
 });
