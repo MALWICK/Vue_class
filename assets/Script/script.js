cca93d02dccd6b07748a2d816cb00d2e76ce204b
MALWICK <batahumphrey66@gamil.com>
2023-08-11 12:47:31 +0100

    fetch data from api

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -3,10 +3,26 @@ const app = Vue.createApp({
     return {
       firstname: "John",
       lastName: "Smith",
+      age: 25,
       email: "JohnSmith@gmail.com",
-      gender:'male',
+      gender: "male",
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
     };
+  },
+
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      console.log(results);
+      this.firstname = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.age = results[0].dob.age;
+      this.email = "SamSmith@gmail.com";
+      this.gender = "female";
+      this.picture = "https://randomuser.me/api/portraits/women/10.jpg";
+      /* console.log(this.age); */
+    },
   },
 });
 
